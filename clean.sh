d1616eb9696a8ea8978a3d0976afd39a933eb561
Denis Chancogne <denis.chancogne@free.fr>
2016-01-22 18:52:00 +0100

    Move sources into src directory.

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+THIS_HOME=$(dirname $0)
+
+do_rm() {
+	local ELMT_LIST=$1
+	if [ -n "$ELMT_LIST" ]
+	then
+		echo "REMOVE: [$ELMT_LIST]"
+		rm -rf $ELMT_LIST
+	fi
+}
+
+process_pattern() {
+	local PATTERN=$1
+	local ELMT_LIST=$(find $THIS_HOME -name "$PATTERN" | tr '\n' ' ')
+	do_rm "$ELMT_LIST"
+}
+
+cat $THIS_HOME/.gitignore | while read PATTERN
+do
+	process_pattern "$PATTERN"
+done
