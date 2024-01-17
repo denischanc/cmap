bf9c007a238d98a577c26ff667a12430cf341eb8
Denis Chancogne <denis.chancogne@free.fr>
2024-01-17 10:01:43 +0100

    Add not null ptr assertion.

--- src/util/cmap-assert-define.h
+++ src/util/cmap-assert-define.h
@@ -4,4 +4,7 @@
 #define CMAP_ASSERT(cond, msg_err) \
   cmap_assert_public.assert(cond, __FILE__, __LINE__, msg_err)
 
+#define CMAP_ASSERT_NOT_NULL(ptr) \
+  CMAP_ASSERT(ptr != NULL, #ptr " is null !?")
+
 #endif
