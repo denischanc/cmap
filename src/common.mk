2aa7a24caa51cfe4ef446977a894613d8f6579b0
Denis Chancogne <denis.chancogne@free.fr>
2024-01-22 22:01:07 +0100

    score creation.

--- src/common.mk
+++ src/common.mk
@@ -5,21 +5,20 @@ CMAP_SRC_PATH = $(top_srcdir)/src
 CMAP_BUILD_PATH = $(top_builddir)/src
 
 CMAP_CFLAGS = \
-  -I$(CMAP_SRC_PATH) \
-  -I$(CMAP_SRC_PATH)/parser \
-  -I$(CMAP_SRC_PATH)/util \
-  -I$(CMAP_SRC_PATH)/core \
-  -I$(CMAP_SRC_PATH)/core/prototype \
-  -I$(CMAP_SRC_PATH)/core/cmp \
-  -I$(CMAP_SRC_PATH)/kernel \
-  -I$(CMAP_SRC_PATH)/components \
   -I$(CMAP_SRC_PATH)/components/cli \
   -I$(CMAP_SRC_PATH)/components/console \
   -I$(CMAP_SRC_PATH)/components/scheduler \
-  -I$(CMAP_BUILD_PATH)/parser \
-  -I$(CMAP_BUILD_PATH)/components \
+  -I$(CMAP_SRC_PATH)/components \
+  -I$(CMAP_SRC_PATH)/core/cmp \
+  -I$(CMAP_SRC_PATH)/core/prototype \
+  -I$(CMAP_SRC_PATH)/core/score \
+  -I$(CMAP_SRC_PATH)/core \
+  -I$(CMAP_SRC_PATH)/kernel \
+  -I$(CMAP_SRC_PATH)/util \
+  -I$(CMAP_SRC_PATH) \
   -I$(CMAP_BUILD_PATH)/components/console \
   -I$(CMAP_BUILD_PATH)/components/scheduler \
+  -I$(CMAP_BUILD_PATH)/components \
   -Wall
 
 if CONSUMED_TIME
