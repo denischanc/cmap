4b0bc47420483b52c2fca2b162a6d1f2f3266f3b
Denis Chancogne <denis.chancogne@free.fr>
2025-10-12 01:04:34 +0200

    Parser test005 added.

--- test/parser/tests.mk
+++ test/parser/tests.mk
@@ -3,4 +3,5 @@ TESTS += \
   $(top_builddir)/test/parser/test001 \
   $(top_builddir)/test/parser/test002 \
   $(top_builddir)/test/parser/test003 \
-  $(top_builddir)/test/parser/test004
+  $(top_builddir)/test/parser/test004 \
+  $(top_builddir)/test/parser/test005
